b36a8291b7aa2ba21886f7321e95b1239ec7cbb3
luigithiago <luigithiago13@hotmail.com>
2025-02-01 22:27:59 -0400

    commitando segunda parte da implementaçao

--- calculadoraPartidasRanqueadas.js
+++ calculadoraPartidasRanqueadas.js
@@ -4,21 +4,31 @@ calcularSaldo()
 function calcularSaldo(vitorias, derrotas){
     return vitorias - derrotas
 }
-let saldoVitorias = calcularSaldo(20, 10)
 
-function calcularNivel(saldoVitorias){
-    if (vitorias < 10)return "Ferro"
-Se vitórias for entre 11 e 20 = Bronze
-Se vitórias for entre 21 e 50 = Prata
-Se vitórias for entre 51 e 80 = Ouro
-Se vitórias for entre 81 e 90 = Diamante
-Se vitórias for entre 91 e 100= Lendário
-Se vitórias for maior ou igual a 101 = Imortal
+
+function calcularNivel(vitorias){
+    if (vitorias < 10)return "Ferro";
+    else if (vitorias >= 11 && vitorias <= 20) return "Bronze";
+    else if (vitorias >= 21 && vitorias <= 50) return  "Prata";
+    else if (vitorias >= 51 && vitorias <= 80) return  "Ouro";
+    else if (vitorias >= 81 && vitorias <= 90) return  "Diamante";
+    else if (vitorias >=91 && vitorias <= 100) return "Lendário"
+    else return "Imortal";
 }
+
+for (let nivel in niveis){
+  if (vitorias >= nivel.min && vitorias <= nivel){
+    return nivel;
+  }
+}
+
+let saldoVitorias = calcularSaldo(30, 8)
+let nivel = calcularNivel()
+
 
 
 
 
   // Imprime o resultado no console
-  console.log(`O Herói tem de saldo de ${saldoVitorias}`)
+  console.log(`O Herói tem de saldo de ${saldoVitorias} está no nível de ${nivel}`)
  
